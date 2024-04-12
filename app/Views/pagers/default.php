498a3c50d32f7c54034acda5621eb4fbd589a67a
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-04-12 22:00:09 +0700

    removing index page in url, and configuring pagination

---
+++ app/Views/pagers/default.php
@@ -0,0 +1,31 @@
+<?php $pager->setSurroundCount(2) ?>
+
+<nav aria-label="Page navigation">
+   <div
+      class="pagination flex gap-1 [&>*]:w-7 [&>*]:h-7 [&>*[aria-checked=true]]:bg-sky-500 [&>*:not([aria-checked=true])]:bg-white [&>*:not([aria-checked=true])]:text-black [&>*]:rounded-md [&>*]:flex [&>*]:items-center [&>*]:justify-center">
+      <?php if ($pager->hasPrevious()): ?>
+         <a href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>">
+            <span aria-hidden="true"><?= lang('Pager.first') ?></span>
+         </a>
+         <a href="<?= $pager->getPrevious() ?>" aria-label="<?= lang('Pager.previous') ?>">
+            <span aria-hidden="true"><?= lang('Pager.previous') ?></span>
+         </a>
+
+      <?php endif ?>
+
+      <?php foreach ($pager->links() as $link): ?>
+         <a href="<?= $link['uri'] ?>" <?= $link['active'] ? 'aria-checked="true"' : '' ?>>
+            <?= $link['title'] ?>
+         </a>
+      <?php endforeach ?>
+
+      <?php if ($pager->hasNext()): ?>
+         <a href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
+            <span aria-hidden="true"><?= lang('Pager.next') ?></span>
+         </a>
+         <a href="<?= $pager->getLast() ?>" aria-label="<?= lang('Pager.last') ?>">
+            <span aria-hidden="true"><?= lang('Pager.last') ?></span>
+         </a>
+      <?php endif ?>
+   </div>
+</nav>
