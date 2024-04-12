a4a413cdf8dbed4468703dd96cc16c56ab6ef0a4
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-04-12 22:28:06 +0700

    tailwind proper installation

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -5,7 +5,7 @@
    <!-- Required meta tags -->
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <script src="https://cdn.tailwindcss.com"></script>
+   <link href="./assets/css/output.css" rel="stylesheet">
    <title><?= $judul; ?></title>
 
 </head>
