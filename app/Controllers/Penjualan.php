d984f7dfef17469640131b622f3e73c5771d565c
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-04-12 22:01:51 +0700

    improving penjualan methods with try catch and post penjualan views

--- app/Controllers/Penjualan.php
+++ app/Controllers/Penjualan.php
@@ -3,14 +3,11 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
 use App\Models\PenjualanHeaderModel;
 use App\Models\PenjualanDetailModel;
 use App\Models\OutletModel;
 use App\Models\BarangModel;
 use CodeIgniter\I18n\Time;
-
-use function PHPUnit\Framework\isEmpty;
 
 class Penjualan extends BaseController
 {
@@ -80,12 +77,6 @@ class Penjualan extends BaseController
         $qty_array = [];
 
         foreach ($details as $key) {
-            // $barang = [
-            //     $key['kode_barang'] => $key['harga']
-            // ];
-            // $qty = [
-            //     $key['kode_barang'] => $key['qty']
-            // ];
             $barang_array = [
                 ...$barang_array,
                 $key['kode_barang'] => $key['harga']
@@ -110,7 +101,7 @@ class Penjualan extends BaseController
         ];
 
         $data = [
-            "judul" => "Buat Penjualan",
+            "judul" => "Update Penjualan",
             'barangs' => $this->barangModel->findAll(),
             'outlets' => $this->outletModel->findAll(),
             'penjualan' => $penjualan
@@ -121,157 +112,155 @@ class Penjualan extends BaseController
 
     public function addPenjualan()
     {
-        $post_data = $this->request->getPost();
-        $post_data["qty"] = array_filter($post_data['qty'], function ($var) {
-            if ($var) {
-                return $var;
+        try {
+            $post_data = $this->request->getPost();
+            $post_data["qty"] = array_filter($post_data['qty'], function ($var) {
+                if ($var) {
+                    return $var;
+                }
+            });
+
+            if (!$this->validateData($post_data, 'raw_penjualan')) {
+                return redirect()->to('penjualan/add')->withInput();
             }
-        });
 
-        if (!$this->validateData($post_data, 'raw_penjualan')) {
-            return redirect()->to('penjualan/add')->withInput();
-        }
+            $valid_data = $this->validator->getValidated();
 
-        $valid_data = $this->validator->getValidated();
+            // Generate no_faktur
+            $date = date('Y-m-d', strtotime($valid_data['tanggal_faktur']));
+            $time = Time::parse($date);
+            $month = (int) $time->getMonth() < 10 ? '0' . $time->getMonth() : $time->getMonth();
+            $no_faktur_temp = 'FAK-' . $time->getYear() . $month;
 
-        // Generate no_faktur
-        $date = date('Y-m-d', strtotime($valid_data['tanggal_faktur']));
-        $time = Time::parse($date);
-        $month = (int)$time->getMonth() < 10 ? '0' . $time->getMonth() : $time->getMonth();
-        $no_faktur_temp = 'FAK-' . $time->getYear() . $month;
+            $this->builder->like('no_faktur', $no_faktur_temp);
+            $result = $this->builder->get()->getLastRow();
+            $no_faktur = '';
 
-        $this->builder->like('no_faktur', $no_faktur_temp);
-        $result = $this->builder->get()->getLastRow();
-        $no_faktur = '';
+            if (!$result) {
+                $no_faktur = $no_faktur_temp . '-' . '001';
+            } else {
+                $no_faktur = $result->no_faktur;
+                $no_faktur++;
+            }
 
-        // dd($result);
+            $header_data = [
+                'no_faktur' => $no_faktur,
+                'tanggal_faktur' => $valid_data['tanggal_faktur'],
+                'kode_outlet' => $valid_data['kode_outlet'],
+                'amount' => $valid_data['amount'],
+                'discount' => isset($valid_data['discount']) ? $valid_data['discount'] : 0,
+                'ppn' => $valid_data['ppn'],
+                'total_amount' => $valid_data['total_amount'],
+                'created_user' => 'admin',
+                'edit_user' => 'admin',
+            ];
 
-        if (!$result) {
-            $no_faktur = $no_faktur_temp . '-' . '001';
-        } else {
-            $no_faktur = $result->no_faktur;
-            $no_faktur++;
-        }
+            $barangs = $valid_data['barang'];
+            $qty = $valid_data['qty'];
+            $detail_data_array = [];
 
-        $header_data = [
-            'no_faktur' => $no_faktur,
-            'tanggal_faktur' => $valid_data['tanggal_faktur'],
-            'kode_outlet' => $valid_data['kode_outlet'],
-            'amount' => $valid_data['amount'],
-            'discount' => isset($valid_data['discount']) ? $valid_data['discount'] : 0,
-            'ppn' => $valid_data['ppn'],
-            'total_amount' => $valid_data['total_amount'],
-            'created_user' => 'admin',
-            'edit_user' => 'admin',
-        ];
+            foreach ($barangs as $key => $value) {
+                if ($qty[$key] > 0) {
+                    $detail_data = [
+                        'no_faktur' => $no_faktur,
+                        'kode_barang' => $key,
+                        'qty' => $qty[$key],
+                        'harga' => $value,
+                        'sub_total' => $qty[$key] * $value,
+                        'created_user' => 'admin',
+                        'edit_user' => 'admin',
+                    ];
 
-        $barangs = $valid_data['barang'];
-        $qty = $valid_data['qty'];
-        $detail_data_array = [];
-
-        foreach ($barangs as $key => $value) {
-            if ($qty[$key] > 0) {
-                $detail_data = [
-                    'no_faktur' => $no_faktur,
-                    'kode_barang' => $key,
-                    'qty' => $qty[$key],
-                    'harga' => $value,
-                    'sub_total' => $qty[$key] * $value,
-                    'created_user' => 'admin',
-                    'edit_user' => 'admin',
-                ];
-
-                if ($this->validateData($detail_data, 'penjualan_detail')) {
-                    array_push($detail_data_array, $this->validator->getValidated());
+                    if ($this->validateData($detail_data, 'penjualan_detail')) {
+                        array_push($detail_data_array, $this->validator->getValidated());
+                    }
                 }
             }
+
+            if ($this->validateData($header_data, 'penjualan_header')) {
+                $valid_header = $this->validator->getValidated();
+                $this->penjualanHeaderModel->save($valid_header);
+            }
+
+            $this->builder->resetQuery();
+            $this->builder = $this->db->table('penjualan_detail');
+            $this->builder->insertBatch($detail_data_array);
+
+            session()->setFlashdata('message', ['success', 'Penjualan berhasil ditambahkan']);
+            return redirect('penjualan');
+        } catch (\Throwable $th) {
+            session()->setFlashdata('message', ['error', 'Penjualan gagal ditambahkan']);
+            return redirect()->to('/penjualan/add')->withInput();
         }
-
-        if ($this->validateData($header_data, 'penjualan_header')) {
-            $valid_header = $this->validator->getValidated();
-            $this->penjualanHeaderModel->save($valid_header);
-        }
-
-        $this->builder->resetQuery();
-        $this->builder = $this->db->table('penjualan_detail');
-        $this->builder->insertBatch($detail_data_array);
-
-        session()->setFlashdata('message', ['success', 'Penjualan berhasil ditambahkan']);
-        return redirect()->to('/penjualan');
-        // try {
-        // } catch (\Throwable $th) {
-        //     session()->setFlashdata('message', ['error', 'Penjualan gagal ditambahkan']);
-        //     return redirect()->to('/penjualan/add')->withInput();
-        // }
     }
 
     public function updatePenjualan()
     {
-        $post_data = $this->request->getPost();
-        $post_data["qty"] = array_filter($post_data['qty'], function ($var) {
-            if ($var) {
-                return $var;
+        try {
+            $post_data = $this->request->getPost();
+            $post_data["qty"] = array_filter($post_data['qty'], function ($var) {
+                if ($var) {
+                    return $var;
+                }
+            });
+
+            if (!$this->validateData($post_data, 'raw_penjualan')) {
+                return redirect()->to('penjualan/add')->withInput();
             }
-        });
 
-        if (!$this->validateData($post_data, 'raw_penjualan')) {
-            return redirect()->to('penjualan/add')->withInput();
-        }
-
-        $valid_data = $this->validator->getValidated();
+            $valid_data = $this->validator->getValidated();
 
 
-        $header_data = [
-            'no_faktur' => $valid_data["no_faktur"],
-            'tanggal_faktur' => $valid_data['tanggal_faktur'],
-            'kode_outlet' => $valid_data['kode_outlet'],
-            'amount' => $valid_data['amount'],
-            'discount' => isset($valid_data['discount']) ? $valid_data['discount'] : 0,
-            'ppn' => $valid_data['ppn'],
-            'total_amount' => $valid_data['total_amount'],
-            'created_user' => 'admin',
-            'edit_user' => 'admin',
-        ];
+            $header_data = [
+                'no_faktur' => $valid_data["no_faktur"],
+                'tanggal_faktur' => $valid_data['tanggal_faktur'],
+                'kode_outlet' => $valid_data['kode_outlet'],
+                'amount' => $valid_data['amount'],
+                'discount' => isset($valid_data['discount']) ? $valid_data['discount'] : 0,
+                'ppn' => $valid_data['ppn'],
+                'total_amount' => $valid_data['total_amount'],
+                'created_user' => 'admin',
+                'edit_user' => 'admin',
+            ];
 
-        $barangs = $valid_data['barang'];
-        $qty = $valid_data['qty'];
-        $detail_data_array = [];
+            $barangs = $valid_data['barang'];
+            $qty = $valid_data['qty'];
+            $detail_data_array = [];
 
-        foreach ($barangs as $key => $value) {
-            if (isset($qty[$key]) && $qty[$key] > 0) {
-                $detail_data = [
-                    'no_faktur' => $valid_data["no_faktur"],
-                    'kode_barang' => $key,
-                    'qty' => $qty[$key],
-                    'harga' => $value,
-                    'sub_total' => $qty[$key] * $value,
-                    'created_user' => 'admin',
-                    'edit_user' => 'admin',
-                ];
+            foreach ($barangs as $key => $value) {
+                if (isset($qty[$key]) && $qty[$key] > 0) {
+                    $detail_data = [
+                        'no_faktur' => $valid_data["no_faktur"],
+                        'kode_barang' => $key,
+                        'qty' => $qty[$key],
+                        'harga' => $value,
+                        'sub_total' => $qty[$key] * $value,
+                        'created_user' => 'admin',
+                        'edit_user' => 'admin',
+                    ];
 
-                if ($this->validateData($detail_data, 'penjualan_detail')) {
-                    array_push($detail_data_array, $this->validator->getValidated());
+                    if ($this->validateData($detail_data, 'penjualan_detail')) {
+                        array_push($detail_data_array, $this->validator->getValidated());
+                    }
                 }
             }
+
+            if ($this->validateData($header_data, 'penjualan_header')) {
+                $valid_header = $this->validator->getValidated();
+                $this->penjualanHeaderModel->save($valid_header);
+            }
+
+            $this->penjualanDetailModel->where('no_faktur', $valid_data["no_faktur"])->delete();
+            $this->builder->resetQuery();
+            $this->builder = $this->db->table('penjualan_detail');
+            $this->builder->insertBatch($detail_data_array);
+
+            session()->setFlashdata('message', ['success', 'Penjualan berhasil diperbaharui']);
+            return redirect()->to('/penjualan');
+        } catch (\Throwable $th) {
+            session()->setFlashdata('message', ['error', 'Penjualan gagal ditambahkan']);
+            return redirect()->to('/penjualan/add')->withInput();
         }
-
-        if ($this->validateData($header_data, 'penjualan_header')) {
-            $valid_header = $this->validator->getValidated();
-            $this->penjualanHeaderModel->save($valid_header);
-        }
-
-        $this->penjualanDetailModel->where('no_faktur', $valid_data["no_faktur"])->delete();
-        $this->builder->resetQuery();
-        $this->builder = $this->db->table('penjualan_detail');
-        $this->builder->insertBatch($detail_data_array);
-
-        session()->setFlashdata('message', ['success', 'Penjualan berhasil diperbaharui']);
-        return redirect()->to('/penjualan');
-        // try {
-        // } catch (\Throwable $th) {
-        //     session()->setFlashdata('message', ['error', 'Penjualan gagal ditambahkan']);
-        //     return redirect()->to('/penjualan/add')->withInput();
-        // }
     }
 
     public function deletePenjualan($no_faktur)
@@ -295,7 +284,8 @@ class Penjualan extends BaseController
             $this->builder->orLike('harga', $match);
             $barangs = $this->builder->get()->getResultArray();
 
-            if (count($barangs) == 0) echo "Barang tidak ditemukan";
+            if (count($barangs) == 0)
+                echo "Barang tidak ditemukan";
 
             echo view('penjualan/getfilteredbarangs', ['barangs' => $barangs]);
         } catch (\Throwable $th) {
--- app/Views/penjualan/postpenjualan.php
+++ app/Views/penjualan/postpenjualan.php
@@ -3,30 +3,46 @@
 <?php
 $action = isset($penjualan) ? 'updatepenjualan' : 'addpenjualan';
 helper('form');
-// dd($penjualan)
 ?>
 <div class="pt-5 pb-10 w-full max-w-[600px] mx-auto px-10">
-   <h2 class="mt-5 mb-10 text-2xl font-semibold"><?= $judul ?></h2>
+   <h2 class="mt-5 mb-10 text-2xl font-semibold">
+      <?= $judul ?>
+   </h2>
    <div class="w-full mx-auto bg-zinc-800 p-5 rounded-md text-black">
       <form action="<?= base_url($action) ?>" method="post">
          <?= csrf_field() ?>
-         <?php if (isset($penjualan)) : ?>
+         <?php if (isset($penjualan)): ?>
             <input type="hidden" name="id" value="<?= $penjualan['id'] ?>">
             <input type="hidden" name="no_faktur" value="<?= $penjualan['no_faktur'] ?>">
          <?php endif; ?>
          <div class="flex flex-col gap-2 mb-4">
             <label for="tanggal_faktur" class="text-white font-semibold">Tanggal Faktur</label>
-            <input id="tanggal_faktur" type="date" class="py-1 px-2 rounded-md" name="tanggal_faktur" value="<?= old("tanggal_faktur", isset($penjualan) ? $penjualan["tanggal_faktur"] : "") ?>">
+            <input id="tanggal_faktur" type="date" class="py-1 px-2 rounded-md" name="tanggal_faktur"
+               value="<?= old("tanggal_faktur", isset($penjualan) ? $penjualan["tanggal_faktur"] : "") ?>">
             <span class="invalid text-sm text-red-500">
                <?= validation_show_error('tanggal_faktur') ?>
             </span>
          </div>
          <div class="flex flex-col gap-2 my-4">
             <label for="kode_outlet" class="text-white font-semibold">Outlet</label>
-            <select data-old="<?= old("kode_outlet", isset($penjualan) ? $penjualan["kode_outlet"] : "") ?>" name="kode_outlet" id="kode_outlet" class="bg-white py-1 px-2 rounded-md [&>*]:text-sm">
-               <option value="null" selected disabled>Select outlet</option>
-               <?php foreach ($outlets as $outlet) : ?>
-                  <option value="<?= $outlet["kode_outlet"] ?>"><?= $outlet["nama_outlet"] . " - " . $outlet["kode_outlet"] ?></option>
+            <select data-old="<?= old("kode_outlet", isset($penjualan) ? $penjualan["kode_outlet"] : "") ?>"
+               name="kode_outlet" id="kode_outlet" class="bg-white py-1 px-2 rounded-md [&>*]:text-sm">
+               <option value="null" disabled <?= !old("kode_outlet") || !isset($penjualan) ? "selected" : "" ?>>Select
+                  outlet</option>
+               <?php foreach ($outlets as $outlet): ?>
+                  <?php if (isset($penjualan)): ?>
+                     <option value="<?= $outlet["kode_outlet"] ?>" <?= $penjualan["kode_outlet"] == $outlet["kode_outlet"] ? "selected" : "" ?>>
+                        <?= $outlet["nama_outlet"] . " - " . $outlet["kode_outlet"] ?>
+                     </option>
+                  <?php elseif (old("kode_outlet") && !isset($penjualan)): ?>
+                     <option value="<?= $outlet["kode_outlet"] ?>" <?= old("kode_outlet") == $outlet["kode_outlet"] ? "selected" : "" ?>>
+                        <?= $outlet["nama_outlet"] . " - " . $outlet["kode_outlet"] ?>
+                     </option>
+                  <?php else: ?>
+                     <option value="<?= $outlet["kode_outlet"] ?>">
+                        <?= $outlet["nama_outlet"] . " - " . $outlet["kode_outlet"] ?>
+                     </option>
+                  <?php endif; ?>
                <?php endforeach; ?>
             </select>
             <span class="invalid text-sm text-red-500">
@@ -35,29 +51,41 @@ helper('form');
          </div>
          <div class="flex flex-col gap-2 my-4 group relative">
             <label for="search_barang" class="text-white font-semibold">Barang</label>
-            <input id="search_barang" type="text" placeholder="Cari barang" class="py-1 px-2 rounded-md peer" onkeyup="debounceFilterBarangs()">
-            <?php if (isset($barangs)) : ?>
-               <div id="list_barang" class="w-full bg-white absolute translate-y-[4.5rem] hidden flex-col gap-2 max-h-[300px] overflow-y-auto p-2 rounded-md shadow-md peer-focus:flex hover:flex">
-                  <?php foreach ($barangs as $barang) : ?>
-                     <label for="<?= 'option-' . $barang["kode_barang"] ?>" class="flex justify-between items-center p-1 text-sm">
-                        <input type="checkbox" data-kode_barang="<?= $barang["kode_barang"] ?>" name="barang[<?= $barang['kode_barang'] ?>]" id="<?= 'option-' . $barang["kode_barang"] ?>" value="<?= $barang['harga'] ?>" class="hidden peer option-barang" data-old="<?= old("barang." . $barang["kode_barang"], isset($penjualan["barang"][$barang['kode_barang']]) ? $penjualan["barang"][$barang['kode_barang']] : "") ?>">
+            <input id="search_barang" type="text" placeholder="Cari barang" class="py-1 px-2 rounded-md peer"
+               onkeyup="debounceFilterBarangs()">
+            <?php if (isset($barangs)): ?>
+               <div id="list_barang"
+                  class="w-full bg-white absolute translate-y-[4.5rem] hidden flex-col gap-2 max-h-[300px] overflow-y-auto p-2 rounded-md shadow-md peer-focus:flex hover:flex">
+                  <?php foreach ($barangs as $barang): ?>
+                     <label for="<?= 'option-' . $barang["kode_barang"] ?>"
+                        class="flex justify-between items-center p-1 text-sm">
+                        <input type="checkbox" data-kode_barang="<?= $barang["kode_barang"] ?>"
+                           name="barang[<?= $barang['kode_barang'] ?>]" id="<?= 'option-' . $barang["kode_barang"] ?>"
+                           value="<?= $barang['harga'] ?>" class="hidden peer option-barang"
+                           data-old="<?= old("barang." . $barang["kode_barang"], isset($penjualan["barang"][$barang['kode_barang']]) ? $penjualan["barang"][$barang['kode_barang']] : "") ?>">
 
-                        <span><?= $barang["nama_barang"] . " - Rp. " . number_format($barang['harga'], 0, ',', '.') ?></span>
+                        <span>
+                           <?= $barang["nama_barang"] . " - Rp. " . number_format($barang['harga'], 0, ',', '.') ?>
+                        </span>
 
-                        <div class="h-5 w-5 rounded-full border flex items-center justify-center text-white border-black/40 transition-all duration-300 peer-checked:bg-sky-500">
-                           <svg class="stroke-current h-[14px] w-[14px]" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <div
+                           class="h-5 w-5 rounded-full border flex items-center justify-center text-white border-black/40 transition-all duration-300 peer-checked:bg-sky-500">
+                           <svg class="stroke-current h-[14px] w-[14px]" viewBox="0 0 24 24" fill="none"
+                              xmlns="http://www.w3.org/2000/svg">
                               <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                               <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                               <g id="SVGRepo_iconCarrier">
-                                 <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                 <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
                               </g>
                            </svg>
                         </div>
                      </label>
                   <?php endforeach ?>
                </div>
-            <?php else : ?>
-               <div class="w-full h-fit p-2 bg-white rounded-md text-center text-sm">Tidak ada barang untuk ditampilkan. Harap tambahkan barang terlebih dahulu</div>
+            <?php else: ?>
+               <div class="w-full h-fit p-2 bg-white rounded-md text-center text-sm">Tidak ada barang untuk ditampilkan.
+                  Harap tambahkan barang terlebih dahulu</div>
             <?php endif; ?>
             <span class="invalid text-sm text-red-500">
                <?= validation_show_error('barang') ?>
@@ -65,12 +93,18 @@ helper('form');
          </div>
          <div id="selected-barangs" class="hidden flex-col gap-2 my-4 max-h-[300px] overflow-y-auto">
             <h3 class="text-white font-semibold">Jumlah Barang</h3>
-            <?php if (isset($barangs)) : ?>
-               <?php foreach ($barangs as $barang) : ?>
-                  <div id="<?= 'display-' . $barang['kode_barang'] ?>" class="w-full p-2 rounded-md bg-white hidden justify-between items-center">
-                     <span><?= $barang["nama_barang"] . ' - Rp.' . number_format($barang["harga"], 0, ',', '.') ?></span>
+            <?php if (isset($barangs)): ?>
+               <?php foreach ($barangs as $barang): ?>
+                  <div id="<?= 'display-' . $barang['kode_barang'] ?>"
+                     class="w-full p-2 rounded-md bg-white hidden justify-between items-center">
+                     <span>
+                        <?= $barang["nama_barang"] . ' - Rp.' . number_format($barang["harga"], 0, ',', '.') ?>
+                     </span>
 
-                     <input type="number" min="0" data-harga="<?= $barang["harga"] ?>" name="qty[<?= $barang["kode_barang"] ?>]" id="qty-<?= $barang["kode_barang"] ?>" class="w-[80px] py-1 px-2 text-white bg-zinc-800 rounded-md qty-input" value="<?= old("qty." . $barang["kode_barang"], isset($penjualan["qty"][$barang["kode_barang"]]) ? $penjualan["qty"][$barang["kode_barang"]] : "") ?>">
+                     <input type="number" min="0" data-harga="<?= $barang["harga"] ?>"
+                        name="qty[<?= $barang["kode_barang"] ?>]" id="qty-<?= $barang["kode_barang"] ?>"
+                        class="w-[80px] py-1 px-2 text-white bg-zinc-800 rounded-md qty-input"
+                        value="<?= old("qty." . $barang["kode_barang"], isset($penjualan["qty"][$barang["kode_barang"]]) ? $penjualan["qty"][$barang["kode_barang"]] : "") ?>">
                   </div>
                <?php endforeach ?>
             <?php endif; ?>
@@ -78,14 +112,16 @@ helper('form');
          <div class="grid grid-cols-2 gap-3">
             <div class="flex flex-col gap-2 my-4">
                <label for="discount" class="text-white font-semibold">Discount</label>
-               <input id="discount" min="0" max="0" type="number" class="py-1 px-2 rounded-md" name="discount" value="<?= old("discount", isset($penjualan) ? $penjualan["discount"] : "") ?>">
+               <input id="discount" min="0" max="0" type="number" class="py-1 px-2 rounded-md" name="discount"
+                  value="<?= old("discount", isset($penjualan) ? $penjualan["discount"] : "") ?>">
                <span class="invalid text-sm text-red-500">
                   <?= validation_show_error('discount') ?>
                </span>
             </div>
             <div class="flex flex-col gap-2 my-4">
                <label for="amount" class="text-white font-semibold">Amount</label>
-               <input readonly id="amount" type="text" class="py-1 px-2 rounded-md" name="amount" value="<?= old("amount", isset($penjualan) ? $penjualan["amount"] : "") ?>">
+               <input readonly id="amount" type="text" class="py-1 px-2 rounded-md" name="amount"
+                  value="<?= old("amount", isset($penjualan) ? $penjualan["amount"] : "") ?>">
                <span class="invalid text-sm text-red-500">
                   <?= validation_show_error('amount') ?>
                </span>
@@ -94,14 +130,16 @@ helper('form');
          <div class="grid grid-cols-2 gap-3">
             <div class="flex flex-col gap-2 my-4">
                <label for="ppn" class="text-white font-semibold">PPN (10%)</label>
-               <input readonly id="ppn" type="text" class="py-1 px-2 rounded-md" name="ppn" value="<?= old("ppn", isset($penjualan) ? $penjualan["ppn"] : "") ?>">
+               <input readonly id="ppn" type="text" class="py-1 px-2 rounded-md" name="ppn"
+                  value="<?= old("ppn", isset($penjualan) ? $penjualan["ppn"] : "") ?>">
                <span class="invalid text-sm text-red-500">
                   <?= validation_show_error('ppn') ?>
                </span>
             </div>
             <div class="flex flex-col gap-2 my-4">
                <label for="total_amount" class="text-white font-semibold">Total Amount</label>
-               <input readonly id="total_amount" type="text" class="py-1 px-2 rounded-md" name="total_amount" value="<?= old("total_amount", isset($penjualan) ? $penjualan["total_amount"] : "") ?>">
+               <input readonly id="total_amount" type="text" class="py-1 px-2 rounded-md" name="total_amount"
+                  value="<?= old("total_amount", isset($penjualan) ? $penjualan["total_amount"] : "") ?>">
                <span class="invalid text-sm text-red-500">
                   <?= validation_show_error('total_amount') ?>
                </span>
